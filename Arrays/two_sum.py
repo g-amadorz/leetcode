406099ccce0a039827d50da5e108c5696fa0465d
Gabriel Amador-Zarza <gabriel.amadorz@gmail.com>
2024-11-06 11:24:18 -0800

    up to date

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -5,6 +5,6 @@ class Solution(object):
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
-                return [prevMap[diff], i]
+                return [prevMap[diff], i]   
             prevMap[n] = i
         return
