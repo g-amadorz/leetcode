2a8da12f84acfd093e95ce291f25370653340b18
g-amadorz <gabe.amadorz@gmail.com>
2025-01-26 16:55:34 -0800

    Done N.20 Valid Parentheses

---
+++ Stacks/ValidParentheses.java
@@ -0,0 +1,16 @@
+package Stacks;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        //Stack<String> chars = [];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.println(c);
+        }
+        
+        return false;
+    }
+    
+}
