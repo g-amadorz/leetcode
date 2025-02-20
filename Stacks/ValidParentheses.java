ebbd48ddb98010fb2d4fa2d44043f5e17d260d96
g-amadorz <gabe.amadorz@gmail.com>
2025-02-20 11:36:44 -0800

    hello

--- Stacks/ValidParentheses.java
+++ Stacks/ValidParentheses.java
@@ -1,16 +1,42 @@
 package Stacks;
 
-public class ValidParentheses {
+import java.util.ArrayList;
+import java.util.Stack;
 
+public class ValidParentheses {
     public boolean isValid(String s) {
-        //Stack<String> chars = [];
+        Stack<Character> chars = new Stack<Character>();
+
+        ArrayList<String> validPairs = new ArrayList<String>();
+
+        //Character init = s.charAt(0);
+
+        //chars.push(init);
+
+        validPairs.add("()");
+        validPairs.add("{}");
+        validPairs.add("[]");
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            System.out.println(c);
+
+            if (chars.isEmpty()) {
+                chars.push(c);
+            } else {
+                char d = chars.peek();
+
+                String check = d + "" + c;
+
+                System.out.println(check);
+
+                if (validPairs.contains(check)) {
+                    chars.pop();
+                } else {
+                    chars.push(c);
+                }
         }
-        
-        return false;
+            }
+
+        return chars.isEmpty();
     }
-    
 }
